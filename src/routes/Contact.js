4fe2a9a39c4472eaf813aa3d8f2f8f4cbe0e751e
ZhangXiao <xiao-zhang@siemens.com>
2018-12-11 22:57:06 +0800

    react-router demo

---
+++ src/routes/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Contact extends React.Component {
+    render(){
+        return(
+            <div>
+                <h2>Got questions ?</h2>
+                <p>
+                    the easiest thing to do is post on our
+                    <a href="https://github.com/shaw1121">github</a>
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
